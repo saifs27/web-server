88ae201a5c49ce1ff4e8842570802645ab3e0123
Saif <113651806+saifs27@users.noreply.github.com>
2024-01-14 01:08:58 -0600

    initial commit

---
+++ webserver/webserver.hpp
@@ -0,0 +1,11 @@
+﻿#pragma once
+
+#ifdef _WIN32
+#define _WIN32_WINNT 0X0A00
+#endif
+#define ASIO_STANDALONE
+
+#include <iostream>
+#include <asio.hpp>
+
+// TODO: Reference additional headers your program requires here.
